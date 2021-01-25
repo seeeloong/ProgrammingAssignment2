053febe6e0326e9b1d0764be90116d6a0c5e1a95
seeeloong <77083934+seeeloong@users.noreply.github.com>
2021-01-25 21:02:24 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,8 @@
 ## makeCacheMatrix is a list of 4 functions, 
 ## Set: which set the input matrix as an argument for later use
 ## Get: retrieve input matrix for later calculation
-## Setmean: save the previously calculated inverse matrix as cache
-## Getmean: retrieve the previously calculated inverse matrix
+## Setinv: stores the previously calculated inverse matrix as cache
+## Getinv: retrieve the previously calculated inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -14,43 +14,31 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) {
-    m <<- mean
+  setinv <- function(solve) {
+    m <<- solve
   }
-  getmean <- function() m
+  getinv <- function() m
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinv = setinv,
+       getinv = getinv)
   
 }
 
 ## cacheSolve is for executing the functions to calculate inverse matrix
 ## if there is a matrix already stored (calculated previously), it will output the previously calculated inverse matrix
 ## if there is a new matrix input (i.e. no previously calculated output, m = NULL), data will take the new matrix input by x$get()
-## then calculate(data,...) will call the function 'calculate' defined below, to calculate inverse matrix
-## x$setmean(m) will store the new inverse matrix as cache
+## then solve(data,...) will calculate inverse matrix
+## x$setinv(m) will store the new inverse matrix as cache
 ## at last, the inverse matrix 'm' will be printed as output
 
 cacheSolve <- function(x, ...) {
-  m <- x$getmean()
+  m <- x$getinv()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
-  m <- calculate(data, ...)
-  x$setmean(m)
+  m <- solve(data, ...)
+  x$setinv(m)
   m
-}
-
-calculate<-function(x){
-  a<-x[1,1]
-  b<-x[2,1]
-  c<-x[1,2]
-  d<-x[2,2]
-  f<-(1)/((a*d) - (b*c))
-  x[1,1]<-d*f
-  x[2,1]<--b*f
-  x[1,2]<--c*f
-  x[2,2]<-a*f
 }
