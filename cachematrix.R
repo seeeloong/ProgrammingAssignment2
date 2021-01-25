5126546048b5df544d411fa3d49f1ccf2606d15f
seeeloong <77083934+seeeloong@users.noreply.github.com>
2021-01-25 20:18:14 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,44 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) {
+    m <<- mean
+  }
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+  
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- calculate(data, ...)
+  x$setmean(m)
+  m
+}
+
+calculate<-function(x){
+  a<-x[1,1]
+  b<-x[2,1]
+  c<-x[1,2]
+  d<-x[2,2]
+  f<-(1)/((a*d) - (b*c))
+  x[1,1]<-d*f
+  x[2,1]<--b*f
+  x[1,2]<--c*f
+  x[2,2]<-a*f
 }
